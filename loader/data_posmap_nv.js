b25e7dd7e56b687669104870982a9068eac80248
Sigit Prabowo <sgt@prbw.net>
2020-04-29 18:29:46 +0700

    fix: read file direct compressed

--- loader/data_posmap_nv.js
+++ loader/data_posmap_nv.js
@@ -4,12 +4,9 @@ import LZUTF8 from 'lzutf8'
 let textFile
 let fileName = 'posmap_nv.txt'
 
-if (!fs.existsSync(__dirname + '/../data/' + fileName)) {
-  let tmp = fs.readFileSync(__dirname + '/../data/lzutf8.' + fileName, 'utf8')
-  let decompressedBytesAsDecimalString = LZUTF8.Encoding.BinaryString.decode(tmp)
-  let decompressedBytes = LZUTF8.decompress(decompressedBytesAsDecimalString)
-  fs.writeFileSync(__dirname + '/../data/' + fileName, decompressedBytes, 'utf8')
-}
-textFile = fs.readFileSync(__dirname + '/../data/' + fileName, 'utf8');
+let tmp = fs.readFileSync(__dirname + '/../data/lzutf8.' + fileName, 'utf8')
+let decompressedBytesAsDecimalString = LZUTF8.Encoding.BinaryString.decode(tmp)
+let decompressedBytes = LZUTF8.decompress(decompressedBytesAsDecimalString)
+textFile = decompressedBytes
 
 export default textFile;
